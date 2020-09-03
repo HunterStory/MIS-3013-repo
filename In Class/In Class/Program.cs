87032d0c4f889220d11491aa99b83fb78ee3a3e1
HunterStory <hunterann5@ou.edu>
2020-09-03 15:10:38 -0500

    completed Sum of 3

---
+++ In Class/In Class/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConditionalStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double weight = 0;
+            double distance = 0;
+            string hazardousResponse = "";
+            bool containsHazardous = false;
+
+            Console.WriteLine("What is the weight of your shipment in pounds? >>");
+            response = Console.ReadLine();
+            weight = Convert.ToDouble(response);
+
+            Console.WriteLine("What is the distance of your shipment in miles? >>");
+            distance = Convert.ToDouble(Console.readline);
+
+            Console.WriteLine("Does your ship contain hazardous materials, yes or no? >>")
+            response = Console.ReadLine();
+
+            if ()
+
+        }
+    }
+}
---
+++ SumOf3/SumOf3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Dynamic;
+using System.Globalization;
+
+namespace SumOf3
+{
+    class Program
+    {
+        const double LUCKY_NUMBER = 7.777;
+        private const string V = "N3";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter your first number >>");
+            string firstnumber = Console.ReadLine();
+            double first = Convert.ToDouble(firstnumber);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter your second number >>");
+            string secondnumber = Console.ReadLine();
+            double second = Convert.ToDouble(secondnumber);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter your third number >>");
+            string thirdnumber = Console.ReadLine();
+            double third = Convert.ToDouble(thirdnumber);
+
+            double sum = first + second + third;
+
+            Console.WriteLine();
+            Console.WriteLine("The sum of your three numbers is: " + sum.ToString("N3"));
+
+            Console.WriteLine();
+            Console.WriteLine("The sum of your three numbers multiplied by the lucky number is :" + sum * LUCKY_NUMBER);
+        }
+    }
+}
