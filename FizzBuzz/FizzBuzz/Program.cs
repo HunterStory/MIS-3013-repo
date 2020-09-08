c9b48e41f2f413720eb8c335ba3dfd44338bd082
HunterStory <hunterann5@ou.edu>
2020-09-08 12:18:40 -0500

    Changed order

--- FizzBuzz/FizzBuzz/Program.cs
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -8,18 +8,17 @@ namespace FizzBuzz
         {
             Random rnd = new Random();
             int number = rnd.Next(1, 101);
-            Console.WriteLine(number);
 
-            if (number % 3 == 0 && number % 5 == 0)
-                Console.WriteLine("FizzBuzz");
-
-            else if (number % 3 == 0)
+            if (number % 3 == 0)
                 Console.WriteLine("Fizz");
 
             else if (number % 5 == 0)
                 Console.WriteLine("Buzz");
 
-            else
+            else if (number % 3 == 0 && number % 5 == 0)
+                Console.WriteLine("FizzBuzz");
+
+            else Console.WriteLine(number);
 
             {
                 
