cc64c11b686f0cb7c8cdecd8d066d5249ac1c7e2
HunterStory <hunterann5@ou.edu>
2020-09-08 12:41:17 -0500

    Started CoinToss

---
+++ CoinToss/CoinToss/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace CoinToss
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            Console.WriteLine("Please pick Heads or Tails");
+            string pick = Console.ReadLine();
+
+            Random rnd = new Random();
+            int number = rnd.Next(0, 2);
+
+            if (number == 0)
+                Console.WriteLine("The coin landed on Heads");
+            else if (number == 1)
+                Console.WriteLine("The coin landed on Tails");
+
+            
+        }
+    }
+}
