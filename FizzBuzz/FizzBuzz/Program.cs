21c0c489496b6c6d53311a94ab55ccef9a04e2da
HunterStory <hunterann5@ou.edu>
2020-09-08 12:10:05 -0500

    Started FizzBuzz

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+            Console.WriteLine(number);
+
+            if (number % 3 == 0 && number % 5 == 0)
+                Console.WriteLine("FizzBuzz");
+
+            else if (number % 3 == 0)
+                Console.WriteLine("Fizz");
+
+            else if (number % 5 == 0)
+                Console.WriteLine("Buzz");
+
+            else
+
+            {
+                
+            }
+
+        }
+    }
+}
