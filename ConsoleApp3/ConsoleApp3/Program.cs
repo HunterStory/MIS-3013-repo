9db12992caf84a353f7552a30cb01f9f5251c68f
HunterStory <hunterann5@ou.edu>
2020-09-22 15:07:17 -0500

    started p string manipulation under console app 3

---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+
+            Console.WriteLine(sentence);
+            Console.WriteLine();
+
+            Console.WriteLine("Choose a word to search for in the sentence above");
+            string search = Console.ReadLine();
+
+            Console.WriteLine("Choose a word to replace " + search + " with");
+            string replace = Console.ReadLine();
+
+           
+
+            Console.ReadKey();
+        }
+    }
+}
