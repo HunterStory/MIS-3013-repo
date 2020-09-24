11a510112e69fc274d4dcda3283152c9f6700536
HunterStory <hunterann5@ou.edu>
2020-09-24 13:44:17 -0500

    finished HW 2

--- Homework 2/Homework 2/Program.cs
+++ Homework 2/Homework 2/Program.cs
@@ -55,17 +55,108 @@ namespace Homework_2
             }
 
 
+            string mark;
+            int markWeight;
+            if (markGrade >= 90)
+            {
+                mark = "A";
+                markWeight = 4;
+            }
+            else if (markGrade >= 80)
+            {  
+                mark = "B";
+                markWeight = 3;
+            }
+            else if (markGrade >= 70)
+            {
+                mark = "C";
+                markWeight = 2;
+            }
+            else if (markGrade >= 60)
+            {
+                mark = "D";
+                markWeight = 1;
+            }
+            else
+            {
+                mark = "F";
+                markWeight = 0;
+            }
 
 
+            string econ;
+            int econWeight;
+            if (econGrade >= 90)
+            {
+                econ = "A";
+                econWeight = 4;
+            }
+            else if (econGrade >= 80)
+            {
+                econ = "B";
+                econWeight = 3;
+            }
+            else if (econGrade >= 70)
+            {
+                econ = "C";
+                econWeight = 2;
+            }
+            else if (econGrade >= 60)
+            {
+                econ = "D";
+                econWeight = 1;
+            }
+            else
+            {
+                econ = "F";
+                econWeight = 0;
+            }
 
 
+            string mis;
+            int misWeight;
+            if (misGrade >= 90)
+            {
+                mis = "A";
+                misWeight = 4;
+            }
+            else if (misGrade >= 80)
+            {
+                mis = "B";
+                misWeight = 3;
+            }
+            else if (misGrade >= 70)
+            {
+                mis = "C";
+                misWeight = 2;
+            }
+            else if (misGrade >= 60)
+            {
+                mis = "D";
+                misWeight = 1;
+            }
+            else
+            {
+                mis = "F";
+                misWeight = 0;
+            }
 
+            Console.WriteLine();
+            Console.WriteLine("Your accounting grade is " + acct);
+            Console.WriteLine($"Your Marketing grade is {mark}");
+            Console.WriteLine($"Your Economics grade is {econ}");
+            Console.WriteLine($"Your MIS grade is {mis}");
 
+            Console.WriteLine();
 
-                Console.WriteLine("Your accounting grade is " + acct);
+            acctweight = acctweight * 3;
+            markWeight = markWeight * 3;
+            econWeight = econWeight * 3;
+            misWeight = misWeight * 3;
 
-                gpa = acctweight;
-                Console.WriteLine($"Your GPA is {gpa}");
+  
+            gpa = (acctweight + markWeight + econWeight + misWeight)/12;
+            Console.WriteLine($"Your GPA is {gpa.ToString("N2")}");
 
 
                 Console.ReadKey();
--- Homework 2/Trivia Quiz/Program.cs
+++ Homework 2/Trivia Quiz/Program.cs
@@ -10,20 +10,71 @@ namespace Trivia_Quiz
     {
         static void Main(string[] args)
         {
-            int firstAns;
+            string first, second, third, fourth, fifth;
+            int firstAns, secondAns, thirdAns, fourthAns, fifthAns;
 
             Console.WriteLine("Answer the following 5 questions to see who is the smartest");
 
+            Console.WriteLine();
+
             Console.WriteLine("The sky is blue. (T/F)");
-            firstAns = Convert.ToInt32(Console.ReadLine());
-            if (firstAns == "T")
+            first = Console.ReadLine().ToLower();
+            if (first == "t")
                 firstAns = 1;
-            else firstAns = 0;
+            else if (first == "f")
+                firstAns = 0;
+            else return;
+            Console.WriteLine();
 
             Console.WriteLine("2 + 2 = 4. (T/F)");
+            second = Console.ReadLine().ToLower();
+            if (second == "t")
+                secondAns = 1;
+            else if (second == "f")
+                secondAns = 0;
+            else return;
+            Console.WriteLine();
+
             Console.WriteLine("Grass is yellow. (T/F)");
+            third = Console.ReadLine().ToLower();
+            if (third == "f")
+                thirdAns = 1;
+            else if (third == "t")
+                thirdAns = 0;
+            else return;
+            Console.WriteLine();
+
             Console.WriteLine("Earth is round. (T/F)");
-            Console.WriteLine("The sun is yellow. (T/F)");
+            fourth = Console.ReadLine().ToLower();
+            if (fourth == "t")
+                fourthAns = 1;
+            else if (fourth == "f")
+                fourthAns = 0;
+            else return;
+            Console.WriteLine();
+
+            Console.WriteLine("The sun is brown. (T/F)");
+            fifth = Console.ReadLine().ToLower();
+            if (fifth == "f")
+                fifthAns = 1;
+            else if (fifth == "t")
+                fifthAns = 0;
+            else return;
+            Console.WriteLine();
+
+            int ans;
+            ans = firstAns + secondAns + thirdAns + fourthAns + fifthAns;
+
+            double ansPercent;
+            ansPercent = Convert.ToDouble(ans) / 5;
+
+
+            Console.WriteLine($"Your score is {ans}/5 or {ansPercent.ToString("P0")}");
+
+            Console.ReadKey();
+
+
+
 
 
 
