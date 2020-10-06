b7e32d0730ca26e6b121faa8f644f66f570cd1d2
HunterStory <hunterann5@ou.edu>
2020-10-06 13:00:05 -0500

    started TimeToBurn- HW 3

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -12,18 +12,33 @@ namespace StringManipulation
         static void Main(string[] args)
         {
             string sentence = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
+            sentence = sentence.ToLower();
 
             Console.WriteLine(sentence);
             Console.WriteLine();
 
             Console.WriteLine("Choose a word to search for in the sentence above");
-            string search = Console.ReadLine();
+            string search = Console.ReadLine().ToLower();
 
             Console.WriteLine("Choose a word to replace " + search + " with");
-            string replace = Console.ReadLine();
+            string newword = Console.ReadLine().ToLower();
 
-           
-
+            if (sentence.Contains(search))
+            {
+                sentence = sentence.Replace(search, newword);
+                Console.WriteLine(sentence);
+            }
+            else
+            {
+                Console.WriteLine($"Sorry, I could not find your word {search}");
+                string word = string.Empty;
+                for (int i = search.Length - 1; i >= 0; i--)
+                {
+                    word = word + search[i];
+                    Console.WriteLine(word);
+                }
+            }
+             
             Console.ReadKey();
         }
     }
---
+++ Homework 3/TimeToBurnCalories/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TimeToBurnCalories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many calories would you like to burn?");
+            double goal = Convert.ToDouble(Console.ReadLine());
+
+            double calories = 3.9;
+            while (true)
+            {
+
+            }
+
+        }
+    }
+}
