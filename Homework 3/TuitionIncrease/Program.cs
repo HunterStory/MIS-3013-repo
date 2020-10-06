4d6d7f9d406a86fbaba706b06b4f0eaf147cd5dc
HunterStory <hunterann5@ou.edu>
2020-10-06 13:37:17 -0500

    Finished TuitionIncrease- HW3

---
+++ Homework 3/TuitionIncrease/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TuitionIncrease
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double tuition = 12000;
+            for (int i = 1; i < 8; i++)
+            {
+                tuition = tuition + (tuition * 0.05);
+                Console.WriteLine($"Tuition after year {i} will be {tuition.ToString("C0")}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
