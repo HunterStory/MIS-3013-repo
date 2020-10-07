3e854eccaa0dedf2d77f4090831b0b81391c547a
HunterStory <hunterann5@ou.edu>
2020-10-06 21:42:27 -0500

    worked on participation

---
+++ ProcessingACSVFile/ProcessingACSVFile/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace ProcessingACSVFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = @"sales_data_sample (1).csv";
+
+            string[] lines = File.ReadAllLines(filepath);
+
+            double sum = 0;
+            double sumfor2003 = 0;
+            double sumfor2004 = 0;
+            double sumfor2005 = 0;
+
+            for (int i = 1; i < lines.length; i++)
+            {
+                string line = lines[i];
+                string[] pieces = line.Split(',');
+                double sale = Convert.ToDouble(pieces[4]);
+                string year = pieces[9];
+
+            }
+        }
+    }
+}
